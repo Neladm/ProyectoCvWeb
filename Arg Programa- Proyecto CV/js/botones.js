3fcd27f97b2c0d3afdb5897ef42937818cadf4d2
Antonela <99229884+Neladm@users.noreply.github.com>
2023-02-05 21:24:31 -0300

    Add files via upload

---
+++ Arg Programa- Proyecto CV/js/botones.js
@@ -0,0 +1,9 @@
+document.getElementById("btnDescargar").addEventListener("click", function(){
+    alert("Gracias por decargar el cv ❤ ")
+});
+
+
+
+document.getElementById("btnEnviar").addEventListener("click", function(){
+   alert("Mensaje enviado ✉")
+}); 
